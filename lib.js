e1e73352cf09e0b4b47653b6d51fb34c2b6d994e
kanishk2008338 <74718388+kanishk2008338@users.noreply.github.com>
2020-12-09 15:09:34 +0530

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,19 +1,5 @@
-function collide(p1,p2){
-
-    if (p1.x - p2.x < p2.width/2 + p1.width/2
-      && p2.x - p1.x < p2.width/2 + p1.width/2
-      ) {
-    //movingRect.shapeColor = "red";
-    //fixedRect.shapeColor = "red";
-    p1.velocityX=0;
-    p1.velocityY=0;
-    p2.velocityX=0;
-    p2.velocityY=0;
-  }
-  else {
-    return false;
-    //movingRect.shapeColor = "green";
-    //fixedRect.shapeColor = "green";
-  }
-  
-  }
+function collide(bullet, wall)
+{ bulletRightEdge=bullet.x +bullet.width; 
+  wallLeftEdge=wall.x;
+  if (bulletRightEdge>=wallLeftEdge) { return true } return false;
+} 
